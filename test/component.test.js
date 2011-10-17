b49bd10da60a334839ac2bace76adae1e360f3ed
mosen <admin@ebrosnan.local>
2011-10-17 13:52:53 +1100

    Switching to es5 strict mode. Fleshed out component test. Component now uses Object.create() per es5. Just testing this design.

--- test/component.test.js
+++ test/component.test.js
@@ -1,3 +1,45 @@
 var Component = require('component').Component,
+    assert    = require('assert'),
     path      = require('path');
 
+module.exports = {
+    'create component from build file does not throw error' : function(beforeExit, assert) {
+        var c = Component('./test/fixtures/build.json');
+    },
+
+    'create component with no arguments does not throw error' : function(beforeExit, assert) {
+        assert.throws(function() {
+            var c = Component();
+        }, Error);
+    },
+
+    'test sourcefiles returns full paths to source files specified in test build' : function(beforeExit, assert) {
+        var c = Component('./test/fixtures/build.json'),
+            files = c.sourcefiles;
+
+        assert.equal(files[0], 'test/fixtures/js/component.js', 'test sourcefiles returns relative path to source files');
+    },
+
+    'test version matches build file version' : function(beforeExit, assert) {
+        var c = Component('./test/fixtures/build.json'),
+            version = c.version;
+
+        assert.equal(version, '@VERSION@', 'test version matches build file version');
+    },
+
+    'test details string matches build file details' : function(beforeExit, assert) {
+        var c = Component('./test/fixtures/build.json'),
+            details = c.details,
+            expected = ", { requires: ['base'], lang: ['en-US'], skinnable: true }";
+
+        assert.equal(details, expected, 'test details string matches build details');
+    },
+
+    'test sourcedir matches expected sourcedir' : function(beforeExit, assert) {
+        var c = Component('./test/fixtures/build.json'),
+            sourcedir = c.sourcedir,
+            expected = "./test/fixtures";
+
+        assert.equal(sourcedir, expected, 'test sourcedir matches expected sourcedir');
+    }
+};
