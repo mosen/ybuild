a360ed7a2795431ee9ee35b4a35ed59bfe1c6e6e
mosen <admin@ebrosnan.local>
2012-04-10 17:22:00 +1000

    Started adding regression test for task options not being parsed.

--- test/cli.test.js
+++ test/cli.test.js
@@ -33,5 +33,20 @@ module.exports = {
                 assert.ok(!path.existsSync(path.join(build_dir, '/component-debug.js')), build_dir + '/component-debug.js should not exist');
             });
         });
+    },
+
+    'test defaults are supplied to jslint task (using bitwise as example)' : function(beforeExit, assert) {
+        var test_dir = path.resolve(path.join(__dirname, './fixtures/test_defaults')),
+            build_dir = path.resolve(path.join(__dirname, './fixtures/build'));
+
+        console.log("Spawning ybuild as a child process");
+        console.log("node " + cli_path);
+        console.log("CWD: " + test_dir);
+        var ybuild = child_process.spawn('node', [cli_path, '.'], { cwd: test_dir, env: process.env });
+        attachConsole(ybuild);
+
+        ybuild.on('exit', function (code) {
+            // TODO: test for output related to "Unexpected '&'"
+        });
     }
 }
--- test/fixtures/js/component.js
+++ test/fixtures/js/component.js
@@ -1,4 +1,4 @@
 // Test File
 
-var x = 1;
+var x = 1 & 1;
 alert(x);
